87bb7f7bf5a8c665174463cd6043c8e23f2846bb
Aleksandr Savgirya <poriesto@hotmail.com>
2024-04-23 12:51:52 +0300

    delete passthrough for usb nic

--- w11-gpu.sh
+++ w11-gpu.sh
@@ -7,4 +7,3 @@ sleep 15
 virsh -c qemu:///system attach-device 'win11-gpu' --file /home/poriesto/Documents/vm-xml/devices/mouse-dareu.xml
 virsh -c qemu:///system attach-device 'win11-gpu' --file /home/poriesto/Documents/vm-xml/devices/adata-ssd.xml
 virsh -c qemu:///system attach-device 'win11-gpu' --file /home/poriesto/Documents/vm-xml/devices/durgod-kbd.xml
-virsh -c qemu:///system attach-device 'win11-gpu' --file /home/poriesto/Documents/vm-xml/devices/realtek-usb-nic.xml
